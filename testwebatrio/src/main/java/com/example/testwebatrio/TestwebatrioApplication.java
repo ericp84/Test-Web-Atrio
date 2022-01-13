622180e2753109e8fd5e878f30dc5f1fd9f696e2
Eric Paoli <ericpaoli65@gmail.com>
2022-01-13 17:19:49 +0100

    mise en place du projet test

---
+++ testwebatrio/src/main/java/com/example/testwebatrio/TestwebatrioApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwebatrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwebatrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestwebatrioApplication.class, args);
+	}
+
+}
---
+++ testwebatrio/src/test/java/com/example/testwebatrio/TestwebatrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testwebatrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestwebatrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
